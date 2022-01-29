881ba11487d7c5dff35987b61f69204728b94df5
MohdSuhail786 <thesuhailansari786246@gmail.com>
2022-01-30 02:15:53 +0530

    fix

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const { compile } = require('../controllers/compilerController/compiler');
 
 router.post('/code/submit',compile)
-router.get('/',(req,res)=>res.json("Welcome to coding worms"))
+router.get('/',(req,res)=>res.json("Welcome"))
 
 
 module.exports = router;
