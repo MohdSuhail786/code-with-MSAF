74a3f546bd0b9968ead5a0261cf0e9f66ecb804b
MohdSuhail786 <thesuhailansari786246@gmail.com>
2022-01-30 02:52:03 +0530

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,23 @@
 import {
   BrowserRouter as Router,
-  useRoutes,
+  Route,
+  Routes,
 } from "react-router-dom";
 import CodeEditor from "./Components/CodeEditor/CodeEditor";
 import Signup from "./Components/Signup/Signup";
 import Signin from "./Components/Signin/Signin";
 
 
-
-
-
 function App() {
-  
-  const RoutingComponent = () => {
-    let routes = useRoutes([
-      { path: "/", element: <CodeEditor /> },
-      { path: "signup", element: <Signup /> },
-      { path: "signin", element: <Signin /> },
-      
-    ]);
-    return routes;
-  };
-
 
   return (
     <div className="App">
       <Router>
-        <RoutingComponent />
+        <Routes>
+          <Route exact path='/' element={<CodeEditor />} />
+          <Route exact path='/signup' element={<Signup />} />
+          <Route exact path='/login' element={<Signin />} />
+        </Routes>
       </Router>
     </div>
   );
