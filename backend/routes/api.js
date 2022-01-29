e1f664691a5f654de59eca450904b0bab73f6ba4
MohdSuhail786 <thesuhailansari786246@gmail.com>
2022-01-30 02:05:52 +0530

    added new route

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const { compile } = require('../controllers/compilerController/compiler');
 
 router.post('/code/submit',compile)
+router.get('/',(req,res)=>res.json("Welcome to coding worms"))
 
 
 module.exports = router;
